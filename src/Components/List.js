d2cc693da706f1f1926dd5ab2118d8ab60b751c7
Pritam Panda <89348093+Pritam0077@users.noreply.github.com>
2022-06-17 20:15:38 +0530

    Made the App

---
+++ src/Components/List.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import Todo from "./Todo";
+
+function List() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return; // it will not consider any spaces or gaps in the input
+    }
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    // console.log(todo,...todos)
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return; // it will not consider any spaces or gaps in the input
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo's List (Make your day)</h1>
+      <Form onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+}
+
+export default List;
